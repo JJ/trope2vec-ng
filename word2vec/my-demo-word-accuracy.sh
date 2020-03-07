8791b7233287f90f8fae5df657cfefb9261da161
Jose Carpio <jose.carpio@gmail.com>
2020-03-07 10:41:50 +0000

    added script to compute accuracy to word2vec folder fixes #42

---
+++ word2vec/my-demo-word-accuracy.sh
@@ -0,0 +1,2 @@
+./compute-accuracy new_corpus_v3.bin < new-corpus-accuracy-check-list-10000.txt
+# to compute accuracy with the full vocabulary, use: ./compute-accuracy vectors.bin < questions-words.txt
