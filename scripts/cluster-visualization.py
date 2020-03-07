7e9455231402281384b2c415cb6b4ab4bf82f2df
Jose Carpio <jose.carpio@gmail.com>
2020-03-07 12:40:50 +0000

    cluster visualization

--- scripts/cluster-visualization.py
+++ scripts/cluster-visualization.py
@@ -1,21 +1,26 @@
 # Following instructions from this website
 # https://machinelearningmastery.com/develop-word-embeddings-python-gensim/ 
 
-# from gensim.models import Word2Vec
-
 from sklearn.decomposition import PCA
-
+from matplotlib import pyplot
 import gensim.models.keyedvectors as word2vec
 model = word2vec.KeyedVectors.load_word2vec_format('new_corpus_v3.bin', binary=True)
 
-# model = Word2Vec.load('new_corpus_v3.bin', binary=True)
-
+# define training data
+sentences = [['this', 'is', 'the', 'first', 'sentence', 'for', 'word2vec'],
+			['this', 'is', 'the', 'second', 'sentence'],
+			['yet', 'another', 'sentence'],
+			['one', 'more', 'sentence'],
+			['and', 'the', 'final', 'sentence']]
+# train model
+# model = Word2Vec(sentences, min_count=1)
+# fit a 2d PCA model to the vectors
 X = model[model.wv.vocab]
-
 pca = PCA(n_components=2)
 result = pca.fit_transform(X)
-
-
+# create a scatter plot of the projection
 pyplot.scatter(result[:, 0], result[:, 1])
-
-
+words = list(model.wv.vocab)
+for i, word in enumerate(words):
+	pyplot.annotate(word, xy=(result[i, 0], result[i, 1]))
+pyplot.show()
