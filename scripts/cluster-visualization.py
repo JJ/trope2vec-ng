6924c5462f2a80e7fa9f6cec3848a74a5233ec2d
Jose Carpio <jose.carpio@gmail.com>
2020-03-07 14:18:31 +0100

    added image cluster-36-visualization-new-corpus-v3-1024

--- scripts/cluster-visualization.py
+++ scripts/cluster-visualization.py
@@ -20,7 +20,45 @@ pca = PCA(n_components=2)
 result = pca.fit_transform(X)
 # create a scatter plot of the projection
 pyplot.scatter(result[:, 0], result[:, 1])
-words = list(model.wv.vocab)
-for i, word in enumerate(words):
+# words = list(model.wv.vocab)
+
+words_c1024_0 = ['shrinkingviolet_deadpoetssociety',
+         'visualpun_batmanreturns',
+         'visualpun_brucealmighty', 
+         'visualpun_crocodiledundee',
+         'visualpun_deadpoetssociety',
+         'visualpun_diamondsareforever',
+         'visualpun_herbieridesagain',
+         'visualpun_ironman3',
+         'visualpun_lethalweapon2',
+         'visualpun_marypoppins',
+         'visualpun_nationaltreasure',
+         'visualpun_octopussy',
+         'visualpun_pulpfiction',
+         'visualpun_silentmovie',
+         'visualpun_skyfall',
+         'visualpun_teenagemutantninjaturtles',
+         'visualpun_thedarkknightrises',
+         'visualpun_thefifthelement',
+         'visualpun_thelovebug',
+         'visualpun_themask',
+         'visualpun_themuppetmovie',
+         'visualpun_tradingplaces', 
+         'visualpun_whoframedrogerrabbit',
+         'visualpun_youngfrankenstein']
+
+words_c1024_36 = ['ambiguouslybrown',
+         'archenemy',
+         'callforward',
+         'creativeclosingcredits',
+         'ihaveyounowmypretty',
+         'ironman3',
+         'redeyestakewarning',
+         'shipperondeck',
+         'startofdarkness',
+         'superpowermeltdown',
+         'supersoldier']
+
+for i, word in enumerate(words_c1024_36):
 	pyplot.annotate(word, xy=(result[i, 0], result[i, 1]))
 pyplot.show()
